d72a67adc7f9439566a174aa7dec2853608ca700
Aniket Verma <aniansharyaraj@gmail.com>
2025-05-27 16:44:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,48 @@
-const hourHand = document.querySelector(".hour");
-const minuteHand = document.querySelector(".minute");
-const secondHand = document.querySelector(".second");
-const modeSwitch = document.querySelector(".mode-switch");
-const hourMinSec = document.getElementById("hourMinSec");
-const dayOfTheWeek = document.getElementById("dayOfTheWeek");
-const month = document.getElementById("month");
-const day = document.getElementById("day");
+// Just noticed accessing localStorage is banned from codepen, so disabling saving theme to localStorage
 
-const updateTime = () => {
-    let date = new Date();
-    let hours = date.getHours().toString().padStart(2, '0');
-    let minutes = date.getMinutes().toString().padStart(2, '0');
-    let seconds = date.getSeconds().toString().padStart(2, '0');
-    let dayOfWeek = date.toLocaleString('en-US', { weekday: 'long' });
-    let monthName = date.toLocaleString('default', { month: 'short' });
-    let dayOfMonth = date.getDate().toString().padStart(2, '0');
+const deg = 6;
+const hour = document.querySelector(".hour");
+const min = document.querySelector(".min");
+const sec = document.querySelector(".sec");
 
-    hourMinSec.textContent = `${hours}:${minutes}:${seconds}`;
-    dayOfTheWeek.textContent = dayOfWeek.toUpperCase();
-    month.textContent = monthName.toUpperCase();
-    day.textContent = dayOfMonth;
-    
-    let secToDeg = (date.getSeconds() / 60) * 360;
-    let minToDeg = (date.getMinutes() / 60) * 360;
-    let hrToDeg = (date.getHours() / 12) * 360;
+const setClock = () => {
+  let day = new Date();
+  let hh = day.getHours() * 30;
+  let mm = day.getMinutes() * deg;
+  let ss = day.getSeconds() * deg;
 
-    secondHand.style.transform = `rotate(${secToDeg}deg)`;
-    minuteHand.style.transform = `rotate(${minToDeg}deg)`;
-    hourHand.style.transform = `rotate(${hrToDeg}deg)`;
+  hour.style.transform = `rotateZ(${hh + mm / 12}deg)`;
+  min.style.transform = `rotateZ(${mm}deg)`;
+  sec.style.transform = `rotateZ(${ss}deg)`;
 };
 
-setInterval(updateTime, 1000);
-updateTime();
+// first time
+setClock();
+// Update every 1000 ms
+setInterval(setClock, 1000);
+
+const switchTheme = (evt) => {
+  const switchBtn = evt.target;
+  if (switchBtn.textContent.toLowerCase() === "light") {
+    switchBtn.textContent = "dark";
+    // localStorage.setItem("theme", "dark");
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    switchBtn.textContent = "light";
+    // localStorage.setItem("theme", "light"); //add this
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+};
+
+const switchModeBtn = document.querySelector(".switch-btn");
+switchModeBtn.addEventListener("click", switchTheme, false);
+
+let currentTheme = "dark";
+// currentTheme = localStorage.getItem("theme")
+// 	? localStorage.getItem("theme")
+// 	: null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  switchModeBtn.textContent = currentTheme;
+}
